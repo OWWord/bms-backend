8520d9048037bfe51531aa155f982c65d544db02
Omen1984 <ivan.plehanov42@yandex.ru>
2022-09-29 17:56:40 +0500

    Initial commit

---
+++ src/test/java/com/oww/bmsbackend/BmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oww.bmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
